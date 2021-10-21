5532abddde3ec37b010c284eba9849299f1511e0
Jesse <taochenlei@gmail.com>
2021-10-21 16:24:24 +1000

    add initial form and leads

---
+++ leadmanager/frontend/src/components/Leads/Leads.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Leads extends Component {
+    render() {
+        return (
+            <div>
+                <h1>lead list</h1>
+            </div>
+        )
+    }
+}
+
+export default Leads
