80c6c4315063a687ace3193fa32fdd850f2c66a9
Jesse <taochenlei@gmail.com>
2021-10-28 14:20:17 +1000

    format on save works!

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,20 +1,20 @@
-import React, { Component, Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component, Fragment } from "react";
+import ReactDOM from "react-dom";
 
-import Header from './layout/Header';
-import Dashboard from './Leads/Dashboard';
+import Header from "./layout/Header";
+import Dashboard from "./Leads/Dashboard";
 
 class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <Header />
-                <div className="container">
-                    <Dashboard />
-                </div>
-            </Fragment>
-        )
-    }
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container">
+          <Dashboard />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
